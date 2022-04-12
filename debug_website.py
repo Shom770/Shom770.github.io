c7bbbacdd49133ac72bd7f6ced65ff00e5bd005b
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-04-12 19:45:59 -0400

    left part of home page

--- debug_website.py
+++ debug_website.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 app = Flask(__name__, template_folder=".")
 
 
-@app.route("/all-about-bytecode")
+@app.route("/")
 def home():
     return render_template("all-about-bytecode.html")
 
