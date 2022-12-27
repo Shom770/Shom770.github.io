f8c5d8b6eaebdde1f7d151bef9c9cf8b2c400dfe
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-12-26 20:30:27 -0500

    Index file

--- debug_website.py
+++ debug_website.py
@@ -6,7 +6,7 @@ app = Flask(__name__, template_folder=".")
 
 @app.route("/")
 def home():
-    return render_template("analog-finder.html")
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
