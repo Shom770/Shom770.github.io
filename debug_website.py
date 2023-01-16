9853517c1ebacf0460749f84de702662d35b73f6
Shom770 <82843611+Shom770@users.noreply.github.com>
2023-01-16 17:18:51 -0500

    Add means of sending data across pages w/ JS

--- debug_website.py
+++ debug_website.py
@@ -9,5 +9,9 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/results.html")
+def results():
+    return render_template("results.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
