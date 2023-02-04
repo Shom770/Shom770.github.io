d644e0267d286cef6e59b0f19fad4c8f842017e9
Shom770 <82843611+Shom770@users.noreply.github.com>
2023-02-04 18:56:36 -0500

    Begin building the logic itself.

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-var analogData = JSON.parse(localStorage.getItem("analogData"));
+import naoJson from './teleconnections_data/nao_values.json' assert {type: 'json'};
+
+const analogData = JSON.parse(localStorage.getItem("analogData"));
+
+const naoIndex = null ? analogData["naoIndex"] === "" : parseFloat(analogData["naoIndex"]);
+const aoIndex = null ? analogData["aoIndex"] === "" : parseFloat(analogData["aoIndex"]);
+const epoIndex = null ? analogData["epoIndex"] === "" : parseFloat(analogData["epoIndex"]);
+const pnaIndex = null ? analogData["pnaIndex"] === "" : parseFloat(analogData["pnaIndex"]);
+
 localStorage.removeItem("analogData");
+
+console.log(naoJson);
+// Calculate all analogs
 
 function showSnowfallModal() {
     let fadedBackground = document.getElementById("gray-bg");
     let snowfallModal = document.getElementById("snowfall-modal");
     // Pause old animations
-    fadedBackground.style.webkitAnimationPlayState = "paused";
-    snowfallModal.style.webkitAnimationPlayState = "paused";
+    fadedBackground.style.animationPlayState = "paused";
+    snowfallModal.style.animationPlayState = "paused";
     // Display gray background
-    snowfallModal.style.webkitAnimationPlayState = "running";
+    snowfallModal.style.animationPlayState = "running";
     fadedBackground.style.display = "block";
     // Show modal
     snowfallModal.style.display = "block";
@@ -18,10 +29,10 @@ function closeSnowfallModal() {
     let fadedBackground = document.getElementById("gray-bg");
     let snowfallModal = document.getElementById("snowfall-modal");
     // Pause old animations
-    fadedBackground.style.webkitAnimationPlayState = "paused";
-    snowfallModal.style.webkitAnimationPlayState = "paused";
+    fadedBackground.style.animationPlayState = "paused";
+    snowfallModal.style.animationPlayState = "paused";
     // Close gray background
-    fadedBackground.style.webkitAnimationPlayState = "running";
+    fadedBackground.style.animationPlayState = "running";
     fadedBackground.style.display = "none";
     // Close modal
     snowfallModal.style.display = "none";
@@ -31,10 +42,10 @@ function showIndicesModal() {
     let fadedBackground = document.getElementById("gray-bg");
     let indicesModal = document.getElementById("indices-modal");
     // Pause old animations
-    fadedBackground.style.webkitAnimationPlayState = "paused";
-    indicesModal.style.webkitAnimationPlayState = "paused";
+    fadedBackground.style.animationPlayState = "paused";
+    indicesModal.style.animationPlayState = "paused";
     // Display gray background
-    indicesModal.style.webkitAnimationPlayState = "running";
+    indicesModal.style.animationPlayState = "running";
     fadedBackground.style.display = "block";
     // Show modal
     indicesModal.style.display = "block";
@@ -44,10 +55,10 @@ function closeIndicesModal() {
     let fadedBackground = document.getElementById("gray-bg");
     let indicesModal = document.getElementById("indices-modal");
     // Pause old animations
-    fadedBackground.style.webkitAnimationPlayState = "paused";
-    indicesModal.style.webkitAnimationPlayState = "paused";
+    fadedBackground.style.animationPlayState = "paused";
+    indicesModal.style.animationPlayState = "paused";
     // Close gray background
-    fadedBackground.style.webkitAnimationPlayState = "running";
+    fadedBackground.style.animationPlayState = "running";
     fadedBackground.style.display = "none";
     // Close modal
     indicesModal.style.display = "none";
