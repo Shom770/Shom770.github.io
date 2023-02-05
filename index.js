13aa8faac0d37ed24b479936e74e07ea9492eb1f
Shom770 <82843611+Shom770@users.noreply.github.com>
2023-02-04 20:27:49 -0500

    Analog calculating works!

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ import epoJson from './teleconnections_data/epo_values.json' assert {type: 'json
 import pnaJson from './teleconnections_data/pna_values.json' assert {type: 'json'};
 
 const analogData = JSON.parse(localStorage.getItem("analogData"));
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 if (analogData == null) {
-    alert("It looks like you didn't select a value for any of the indices. In order to find an analog, we need at least ONE index filled out.")
+    alert("It looks like you didn't select a value for any of the indices. In order to find an analog, we need at least ONE index filled out.");
 }
 
 const naoIndex = null ? analogData["naoIndex"] === "" : parseFloat(analogData["naoIndex"]);
@@ -14,11 +15,92 @@ const aoIndex = null ? analogData["aoIndex"] === "" : parseFloat(analogData["aoI
 const epoIndex = null ? analogData["epoIndex"] === "" : parseFloat(analogData["epoIndex"]);
 const pnaIndex = null ? analogData["pnaIndex"] === "" : parseFloat(analogData["pnaIndex"]);
 const stationCode = null ? analogData["stationCode"] === "" : analogData["stationCode"];
+var analogs = [];
 
 localStorage.removeItem("analogData");
 
-console.log(Object.keys(naoJson).length, Object.keys(aoJson).length, Object.keys(epoJson).length, Object.keys(pnaJson).length);
 // Calculate all analogs
+for (let startingYear = 1950; startingYear < 1950 + Object.keys(naoJson).length; startingYear++) {
+    for (let month = 1; month <= 12; month++) {
+        if (4 < month && month < 11) {
+            continue
+        }
+        
+        let desiredIndices = [];
+        let actualIndices = [];
+
+        if (naoIndex !== null) {
+            desiredIndices.push(naoIndex);
+            actualIndices.push(naoJson[startingYear.toString()][month - 1]);
+        }
+
+        if (aoIndex !== null) {
+            desiredIndices.push(aoIndex);
+            actualIndices.push(aoJson[startingYear.toString()][month - 1]);
+        }
+
+        if (epoIndex !== null) {
+            desiredIndices.push(epoIndex);
+            actualIndices.push(epoJson[startingYear.toString()][month - 1]);
+        }
+
+        if (pnaIndex !== null) {
+            desiredIndices.push(pnaIndex);
+            actualIndices.push(pnaJson[startingYear.toString()][month - 1]);
+        }
+        
+        // Using cosine similarity to determine the % of the match between the desired and actual indices.
+        let sumOfBothArraysMultiplied = desiredIndices.map(
+            (enteredIndex, position) => enteredIndex * actualIndices[position]
+        ).reduce((a, b) => a + b, 0);
+
+        let sumOfDesiredIndicesSquared = desiredIndices.map(
+            (enteredIndex) => enteredIndex ** 2
+        ).reduce((a, b) => a + b, 0);
+
+        let sumOfActualIndicesSquared = actualIndices.map(
+            (enteredIndex) => enteredIndex ** 2
+        ).reduce((a, b) => a + b, 0);
+
+        console.log(desiredIndices);
+
+        let matchPercentage = sumOfBothArraysMultiplied / (sumOfDesiredIndicesSquared ** 0.5 * sumOfActualIndicesSquared ** 0.5) * 100;
+
+        analogs.push([startingYear, month, matchPercentage, actualIndices]);
+    }
+}
+
+const analogsSorted = analogs.sort(
+    (a1, a2) => a2[2] - a1[2]
+);
+const bestAnalogs = analogsSorted.slice(0, 5);
+
+console.log(bestAnalogs);
+
+// Display best analogs on screen
+for (let idx = 0; idx < 5; idx++) {
+    let currentAnalog = bestAnalogs[idx];
+    let dateId = document.getElementById(`date-analog-${idx + 1}`);
+    let matchId = document.getElementById(`match-analog-${idx + 1}`);
+    let matchPercentage = currentAnalog[2];
+
+    dateId.innerText = `${months[currentAnalog[1] - 1]} ${currentAnalog[0]}`;
+    matchId.innerText = `${(Math.round(matchPercentage * 10) / 10).toFixed(1)}% Match`
+
+    // Set color of text displaying match based on the match %
+    if (matchPercentage >= 75) {
+        matchId.classList.add("text-emerald-300");
+    }
+    else if (50 <= matchPercentage < 75) {
+        matchId.classList.add("text-yellow-300");
+    }
+    else if (25 <= matchPercentage < 50) {
+        matchId.classList.add("text-orange-300");
+    }
+    else if (matchPercentage < 25) {
+        matchId.classList.add("text-red-300");
+    }
+}
 
 function showSnowfallModal() {
     let fadedBackground = document.getElementById("gray-bg");
